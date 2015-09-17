2fcbc22c3a25dfd89b4d9898e2b84cfffa64e18f
zhankj <zkj19840207@yahoo.com.cn>
2015-09-17 15:37:41 +0800

    alter Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "zkj19840207";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "db6ff1b26775695caea3c0ba89538028"; 
 	
 }
